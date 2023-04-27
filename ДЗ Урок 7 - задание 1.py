289ba650a6f03efce32a7bdbd8d74e8917f07fb0
ValeryHub1 <131370057+ValeryHub1@users.noreply.github.com>
2023-04-27 20:29:55 +0300

    Add files via upload

---
+++ ДЗ Урок 7 - задание 1.py
@@ -0,0 +1,7 @@
+a = input("Введите строку: ")
+b = a[::-1]
+
+if b == a:
+    print("YES")
+else:
+    print("NO")
---
+++ ДЗ Урок 7 - задание 2.py
@@ -0,0 +1,4 @@
+a = str(input("Ввседи строку: "))
+b = " ".join(a.split())
+
+print(b)
